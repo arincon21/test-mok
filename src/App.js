e26f8a59a4907879bf1e4d7e237e10c7b106e411
Alvaro Rincon <arincon@outlook.com>
2023-08-01 08:06:38 -0500

    initial comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import TableUsers from './components/table/table.js';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
+
+  const [usersOriginal, setUsersOriginal] = useState([]);
+  const [usersDelete, setUsersDelete] = useState([]);
+  const [usersVersion, setUsersVersion] = useState([]);
+  const [striped, setStriped] = useState(false);
+
+  useEffect(function () {
+    fetch("https://randomuser.me/api/?page=1&results=100")
+      .then(res => res.json())
+      .then(response => {
+        const users = response.results.map(user => {
+          return { 
+            thumbnail: user.picture.thumbnail,
+            first: user.name.first,
+            last: user.name.last,
+            country: user.location.country
+          }
+        })
+        setUsersOriginal(users)
+        setUsersDelete(users)
+        setUsersVersion(users)
+      })
+  }, [])
+
+  const stripedChange = () => {
+    setStriped(!striped)
+  }
+
+  const orderName = () => { 
+    const newOrder = [...usersDelete].sort((a,b) => (a.first > b.first) ? 1 : ((b.first > a.first) ? -1 : 0))
+    setUsersDelete([...newOrder])
+    setUsersVersion([...newOrder])
+  }
+
+  const orderLastName = () => { 
+    const newOrder = [...usersDelete].sort((a,b) => (a.last > b.last) ? 1 : ((b.last > a.last) ? -1 : 0))
+    setUsersDelete([...newOrder])
+    setUsersVersion([...newOrder])
+  }
+
+  const orderCountry = () => { 
+    const newOrder = [...usersDelete].sort((a,b) => (a.country > b.country) ? 1 : ((b.country > a.country) ? -1 : 0))
+    setUsersDelete([...newOrder])
+    setUsersVersion([...newOrder])
+  }
+
+  const restore = () => {
+    setUsersDelete([...usersOriginal])
+    setUsersVersion([...usersOriginal])
+  }
+
+  const filter = (event) => {
+    const result =  [...usersDelete].filter((user) => user.country.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1)
+    setUsersVersion([...result])
+  }
+
+  const deleteUser = (index) => {
+    const deleteArray = (array, index) => {
+      if (index >= 0 && index < array.length) {
+        array.splice(index, 1);
+        return array
+      } else {
+        console.error("Índice fuera de rango o inválido.");
+      }
+    }
+    const result = deleteArray([...usersDelete], index);
+    setUsersDelete([...result])
+    setUsersVersion([...result])
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Lista de usuarios</h1>
+      <header className='App-header'>
+        <Button variant="primary" onClick={stripedChange}>Colorea filas</Button>
+        <Button variant="primary" onClick={orderCountry}>Ordena por país</Button>
+        <Button variant="primary" onClick={restore} >Restaurar estado inicial</Button>
+        <Form.Control className='imput-mg'  onChange={filter} size="lg" type="text" placeholder="Filtrar por país" />
       </header>
+      <TableUsers 
+        usersAll={usersVersion} 
+        striped={striped} 
+        deleteUser={deleteUser} 
+        orderName={orderName} 
+        orderLastName={orderLastName}
+        orderCountry={orderCountry}
+      
+      ></TableUsers>
     </div>
   );
 }
---
+++ src/components/table/table.js
@@ -0,0 +1,48 @@
+import Table from 'react-bootstrap/Table';
+import Spinner from 'react-bootstrap/Spinner';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+
+function TableUsers(props) {
+    console.log(props);
+    const renderList = () => {
+        if (props.usersAll.length > 0) {
+            return props.usersAll.map((users, index) => <tr key={index}>
+                <td><Image src={users.thumbnail} fluid /></td>
+                <td>{users.first}</td>
+                <td>{users.last}</td>
+                <td>{users.country}</td>
+                <td><Button variant="primary" onClick={() => props.deleteUser(index)}>Delete</Button></td>
+            </tr>)
+        } else {
+            return <tr>
+                <td colSpan={5}>
+                    <Spinner animation="border" role="status">
+                        <span  className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </td>
+            </tr>
+                
+            
+        }
+    }
+
+    return (
+        <Table striped={props.striped} bordered hover>
+            <thead>
+                <tr>
+                    <th >Foto</th>
+                    <th onClick={props.orderName}>Nombre</th>
+                    <th onClick={props.orderLastName}>Apellido</th>
+                    <th onClick={props.orderCountry}>Pais</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderList()}
+            </tbody>
+        </Table>
+    );
+}
+
+export default TableUsers;
